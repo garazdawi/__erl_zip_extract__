1c2a29e3bde603889a5756ab9f709f23cfd6e0ec
Lukas Larsson <lukas@erlang.org>
2022-04-28 08:57:21 +0200

    Add dbg prints

--- test.erl
+++ test.erl
@@ -6,13 +6,22 @@ main(_Argv) ->
   application:ensure_all_started(inets),
   application:ensure_all_started(ssl),
 
-  file:del_dir_r("tmp"),
-  file:make_dir("tmp"),
+  case file:read_file_info(filelib:wildcard("tmp/ImageMagick*")) of
+    {error, _} ->
+      file:del_dir_r("tmp"),
+      file:make_dir("tmp"),
 
-  io:format("Downloading the archive~n"),
-  Url = "https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.1.0-portable-Q16-x64.zip",
-  {ok, {{_, 200, _}, _, Body}} = httpc:request(get, {Url, []}, [], []),
+      io:format("Downloading the archive~n"),
+      Url = "https://download.imagemagick.org/ImageMagick/download/binaries/ImageMagick-7.1.0-portable-Q16-x64.zip",
+      {ok, {{_, 200, _}, _, Body}} = httpc:request(get, {Url, []}, [], []),
+      file:write_file("tmp/ImageMagick-7.1.0-portable-Q16-x64.zip", Body);
+    {ok, _} ->
+      ok
+  end,
 
   io:format("Unzipping~n"),
-  file:write_file("tmp/ImageMagick-7.1.0-portable-Q16-x64.zip", Body),
+
+  dbg:tracer(),dbg:p(all,[call, arity]),
+  dbg:tp(zlib,[]),
+  dbg:tpl(zip, get_z_data, x),
   {ok, _Paths} = zip:extract("tmp/ImageMagick-7.1.0-portable-Q16-x64.zip", [{cwd, "tmp/extract"}]).
